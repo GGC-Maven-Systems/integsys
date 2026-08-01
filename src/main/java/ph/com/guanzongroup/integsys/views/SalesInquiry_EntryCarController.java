d52c747185288bcd8b458e95ea3bf7230eb1845d
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-08-01 15:43:05 +0800

    08012026

--- src/main/java/ph/com/guanzongroup/integsys/views/SalesInquiry_EntryCarController.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/SalesInquiry_EntryCarController.java
@@ -263,6 +263,7 @@ public class SalesInquiry_EntryCarController implements Initializable, ScreenInt
                             if (poSalesInquiryController.SalesInquiry().getDetailCount() > 1) {
                                 if (ShowMessageFX.YesNo(null, pxeModuleName,
                                         "Are you sure you want to change the client?\nPlease note that this action will delete all sales inquiry details.\n\nDo you wish to proceed?") == true) {
+                                    poSalesInquiryController.SalesInquiry().Master().setClientId("");
                                     poSalesInquiryController.SalesInquiry().removeDetails();
                                     loadTableDetail.reload();
                                 } else {
