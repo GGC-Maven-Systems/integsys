d4aa0fd343106ddf58a00ea0d231e1fe35912f40
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-08-21 16:19:24 +0800

    08212026 - Update Inv Request Approval
    - changed setter for tfsourceno from project id into getreferenceno

--- src/main/java/ph/com/guanzongroup/integsys/views/InventoryRequest_ApprovalControllerMP.java
+++ src/main/java/ph/com/guanzongroup/integsys/views/InventoryRequest_ApprovalControllerMP.java
@@ -824,7 +824,7 @@ public class InventoryRequest_ApprovalControllerMP implements Initializable, Scr
         tfCancelQty.setText(tblColCancelQty.getCellData(fnRow));
         tfApprovedQty.setText(tblColApprovedQty.getCellData(fnRow));
         taRemarks.setText(poAppController.getMaster().getRemarks());
-        tfSourceNo.setText(poAppController.getMaster().Project().getProjectID());
+        tfSourceNo.setText(poAppController.getMaster().getReferenceNo());
 
     }
 
